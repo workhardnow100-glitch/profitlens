95ede3592459b8e0627e23d3ca26996bcd72665e
Brendan <workhardnow100@gmail.com>
2025-12-14 08:43:26 +0000

    c

--- pages/mtd-dashboard.js
+++ pages/mtd-dashboard.js
@@ -15,7 +15,7 @@ export default function MtdDashboard() {
     const fetchSummary = async () => {
       setLoading(true);
       try {
-        const res = await fetch("/api/mtd-dashboards", {
+        const res = await fetch("/api/mtd-dashboard", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
