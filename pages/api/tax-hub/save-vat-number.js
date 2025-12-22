8807475f2bdb43cd1c2d9f2f67b3de8d6b218916
Brendan <workhardnow100@gmail.com>
2025-12-22 23:39:53 +0000

    c

--- pages/api/tax-hub/save-vat-number.js
+++ pages/api/tax-hub/save-vat-number.js
@@ -1,5 +1,5 @@
 // pages/api/tax-hub/save-vat-number.js
-import { supabase } from "../../../lib/supabaseClient";
+import { supabase } from "../../../../lib/supabaseClient";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
