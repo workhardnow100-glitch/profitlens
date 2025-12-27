503c08bd30234e4706836f5211d3c520a28d1c3f
Brendan <workhardnow100@gmail.com>
2025-12-27 23:14:17 +0000

    c

--- pages/api/mtd/vat/submit.js
+++ pages/api/mtd/vat/submit.js
@@ -1,6 +1,6 @@
 // pages/api/mtd/vat/submit.js
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../../auth/[...nextauth]";
+import { authOptions } from "../[...nextauth]";
 import { supabaseAdmin } from "../../../../lib/supabase-admin";
 
 const HMRC_BASE_URL = "https://test-api.service.hmrc.gov.uk";
