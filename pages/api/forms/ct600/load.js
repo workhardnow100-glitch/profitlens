9ca1f7c584888c74db1b5dc78a08fdf30d4d0610
Brendan <workhardnow100@gmail.com>
2026-03-31 23:16:50 +0100

    c

--- pages/api/forms/ct600/load.js
+++ pages/api/forms/ct600/load.js
@@ -2,7 +2,7 @@
 
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]";
-import { supabaseAdmin } from "../../../../lib/supabaseAdmin";
+import { supabaseAdmin } from "../../../../lib/supabase-admin";
 
 export default async function handler(req, res) {
   if (req.method !== "GET") {
