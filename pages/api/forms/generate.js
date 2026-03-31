5afdb4a9febc3487c37d5ff3b8f7109a4a77989c
Brendan <workhardnow100@gmail.com>
2026-03-31 19:03:21 +0100

    c

--- pages/api/forms/generate.js
+++ pages/api/forms/generate.js
@@ -29,7 +29,7 @@ import { generateCis300Pdf } from "../../../lib/pdf/templates/cis300";
 import { generateCisStatementPdf } from "../../../lib/pdf/templates/cis_statement";
 
 // CT category map
-import { CT_MAP } from "../../../lib/tax/ct-map";
+import { CT_MAP } from "../../../lib/ct-map";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
